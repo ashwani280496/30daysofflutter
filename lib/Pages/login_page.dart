9164c964b8eeae5ade8006f0150d020263d25694
Ashwani Kumar <ashwani.cse4@gmail.com>
2021-06-20 19:12:23 +0530

    day6 completed [+] statefull widget [+] onState() function use [+] AnimatedContainer [+] async await

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catelog/Helper/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool isButtonPressed = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -20,7 +27,7 @@ class LoginPage extends StatelessWidget {
                 height: 20,
               ),
               Text(
-                'Welcome',
+                'Welcome $name',
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
               ),
               SizedBox(
@@ -34,6 +41,11 @@ class LoginPage extends StatelessWidget {
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: 'Enter Username', labelText: 'Username'),
+                      onChanged: (newText) {
+                        setState(() {
+                          name = newText;
+                        });
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -43,14 +55,38 @@ class LoginPage extends StatelessWidget {
                     SizedBox(
                       height: 20,
                     ),
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, MyRoutes.home);
-                        },
-                        child: Text("Login"),
-                        style: TextButton.styleFrom(
-                          fixedSize: Size(150, 40),
-                        ))
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          isButtonPressed = true;
+                        });
+
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoutes.home);
+                      },
+                      child: AnimatedContainer(
+                          height: 50,
+                          width: isButtonPressed ? 50 : 150,
+                          alignment: Alignment.center,
+                          duration: Duration(seconds: 1),
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.circular(isButtonPressed ? 25 : 8),
+                            color: Colors.purple,
+                          ),
+                          child: isButtonPressed
+                              ? Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : Text(
+                                  'Login',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                )),
+                    ),
                   ],
                 ),
               )
@@ -61,3 +97,12 @@ class LoginPage extends StatelessWidget {
     ));
   }
 }
+
+// ElevatedButton(
+//                           onPressed: () {
+//                             Navigator.pushNamed(context, MyRoutes.home);
+//                           },
+//                           child: Text("Login"),
+//                           style: TextButton.styleFrom(
+//                             fixedSize: Size(150, 40),
+//                           ))
